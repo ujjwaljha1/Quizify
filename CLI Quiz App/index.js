440b9f9e06a9dce61d4692954b712e966e653918
Ujjwal <ujjwaljha744@gmail.com>
2024-06-06 15:07:22 +0530

    Final

--- CLI Quiz App/index.js
+++ CLI Quiz App/index.js
@@ -1,5 +1,11 @@
 /** Creating data Set */
 let readline = require("readline-sync");
+let kuler=require("kuler")
+
+let  userName=readline.question("What is your name: ")
+console.log(kuler(`Hello, ${userName} welcome to Quizify`,"#ca8a04"))
+let score=0;
+
 const database = {
     data: [
         {
@@ -35,7 +41,38 @@ const database = {
     ]
 };
 
-//For Correct answer 
+
+
+const leaderBoard={
+    data:[
+        {
+            name:"Ashish",
+            score: 1
+        },
+        {
+            name:"Riya",
+            score: 3,
+        },
+        {
+            name: "Jay",
+            score: 2,
+        }
+    ]
+}
+
+
+
+
+function playGame(userAnswer,correctAnswer){
+    if(userAnswer === correctAnswer ){
+        console.log(kuler("Correct Answer","#22c55e"))
+        score++
+    }else{
+        console.log(kuler("Incorrect Answer","#b91c1c"))
+        console.log(kuler(`Correct answer is : ${correctAnswer}`,"#0d9488"))
+    }
+}
+
 
 function showQuestionAndOptions() {
     for (let i = 0; i < database.data.length; i++) {
@@ -43,9 +80,22 @@ function showQuestionAndOptions() {
         for (let key in database.data[i].options) {
             console.log(`${key} - ${database.data[i].options[key]}`);
         }
-        let userAnswer = readline.question("Enter your answer: ");
-        
+        let userAnswer = readline.question("Enter your answer: ").toLowerCase();
+        playGame(userAnswer,database.data[i].correctAnswer)
     }
 }
 
+function highScore(leaderBoard){
+    leaderBoard.data.push({name: userName,score:score})
+    let sortedScoreList=leaderBoard.data.sort((a,b) => b.score -a.score)
+    console.log("\nCheck your position on leaderboard!! 😎😎")
+    for(let leader of sortedScoreList){
+        console.log(`${leader.name} - Score: ${leader.score}`)
+    }
+}
+
+
+
 showQuestionAndOptions();
+console.log(`\nYour score is : ${score}`)
+highScore(leaderBoard);
