5459c0d61ce5b4952eec054ead03ab5c40ad633b
Ujjwal <ujjwaljha744@gmail.com>
2024-06-06 14:12:18 +0530

    Js

--- CLI Quiz App/index.js
+++ CLI Quiz App/index.js
@@ -1,9 +1,37 @@
-/**  Creating data Set */
+/** Creating data Set */
 
-cons database={
-    data:[
+const database = {
+    data: [
         {
-            
-        }
+            question: `Let a = {}, b = {}
+            console.log(a == b)
+            console.log(a === b)`,
+            options: {
+                a: "false false",
+                b: "false true",
+                c: "true false",
+                d: "true true"
+            },
+            correctAnswer: "a"
+        },
+        {
+            question: "Object.assign(target, source) creates which type of copy?",
+            options: {
+                a: "Shallow copy",
+                b: "Deep copy",
+                c: "Reference copy",
+                d: "None of the above"
+            },
+            correctAnswer: "a"
+        },
+        question: "Is mesthod chainging possible with forEach?",
+        options: {
+            a: "Yes",
+            b: "No",
+        },
+        correctAnswer: "b"
+    }
     ]
-}
+};
+
+console.log(database);
